b490611eb977e559d91582ec7634d3115d589baa
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2023-01-16 21:09:50 -0300

    Update main.py

--- Graph/main.py
+++ Graph/main.py
@@ -1,13 +1,13 @@
 from graph import Graph
 
 # Criando o grafo
-g1 = Graph(4)
+g1 = Graph(3)
 # Adicionando as arestas
-g1.add_directed_edge(1, 0)
-g1.add_directed_edge(0, 2)
-g1.add_directed_edge(2, 0)
-g1.add_directed_edge(0, 3)
-g1.add_directed_edge(3, 2)
+g1.add_undirected_edge(0, 1)
+g1.add_undirected_edge(0, 2)
+g1.add_undirected_edge(1, 2)
+
+
 # Imprimindo o grafo
 print('GRAPH 1:', g1.adj_list)
 # Imprimindo o grau de saída do nó 0
@@ -20,3 +20,18 @@ print("HIGHEST_DEGREE_OUT:", g1.highest_degree_out())
 print("DEGREE_IN(0):", g1.degree_in(0))
 # Imprimindo o grau de entrada do nó 1
 print("DEGREE_IN(1):", g1.degree_in(1))
+
+print('IS COMPLET: ', g1.is_complete())
+
+
+# Criando outro grafo
+g2 = Graph(3)
+g2.add_directed_edge(0, 1)
+g2.add_directed_edge(0, 2)
+g2.add_undirected_edge(2, 1)
+
+
+print('GRAPH 2:', g2.adj_list)
+print('IS COMPLET: ', g2.is_complete())
+
+help(g2.is_complete)
