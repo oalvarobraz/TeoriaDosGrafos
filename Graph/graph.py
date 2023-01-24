5f69491bfc0608c25a14f10d29516c1e624e346b
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2023-01-24 06:21:37 -0300

    Update graph.py

--- Graph/graph.py
+++ Graph/graph.py
@@ -74,19 +74,101 @@ class Graph:
                 highest_degree_node = i
         return highest_degree_node
 
-    def is_complete(self) -> int:
-        """
-        Returns whether the graph is complete
+    # def is_complete(self) -> int:
+    #     """
+    #     Returns whether the graph is complete
+    #
+    #     :return: 0: ins´t complete
+    #              1: is complete
+    #
+    #     """
+    #     max_degree_out = self.count_nodes - 1
+    #     complete = 0
+    #     for i in range(self.count_nodes):
+    #         if self.degree_out(i) >= max_degree_out:
+    #             complete = 1
+    #         else:
+    #             complete = 0
+    #     return complete
 
-        :return: 0: ins´t complete
-                 1: is complete
-
+    def complete(self):
         """
-        max_degree_out = self.count_nodes - 1
-        complete = 0
-        for i in range(self.count_nodes):
-            if self.degree_out(i) >= max_degree_out:
-                complete = 1
+            Calculetes the density
+        :return: self.node_count*(self.node_count - 1) == self.edge_count
+        """
+        # return self.density() == 1
+        return self.count_nodes * (self.count_nodes - 1) == self.count_edges
+
+    def density(self):
+        return self.count_edges / (self.count_nodes * (self.count_nodes - 1))
+
+    def regular(self):
+        for i in range(1, self.count_nodes):
+            if self.degree_out(i) != self.degree_out(0):
+                return False
+        return True
+
+    def complement(self, g2):
+        """Returns the complent of a graph"""
+        for i in range(len(self.adj_list)):
+            for j in range(len(self.adj_list)):
+                if j not in self.adj_list[i] and j != i and j not in g2.adj_list[i]:
+                    g2.add_undirected_edge(i, j)
+        return g2
+
+    def subgraph(self, g2):
+        """Returns True iif g2 is a subgraph of self"""
+        for i in range(len(g2.adj_list)):
+            for j in range(len(g2.adj_list[i])):
+                if j not in self.adj_list[i]:
+                    return False
+        return True
+
+    def passeio(self, passeio: list):
+        for i in range(len(passeio)-1):
+            if not self.adj_list[passeio[i]].__contains__(passeio[i+1]):
+                return False
+        return True
+
+    def trilha(self, trilha: list):
+        verifica = []
+        for i in range(len(trilha)-1):
+            if not self.adj_list[trilha[i]].__contains__(trilha[i+1]):
+                return False
             else:
-                complete = 0
-        return complete
+                if verifica.__contains__([trilha[i], trilha[i + 1]]) or verifica.__contains__([trilha[i + 1], trilha[i]]):
+                    return False
+            trilha_aux = [trilha[i], trilha[i+1]]
+            verifica.append(trilha_aux)
+        return True
+
+    def circuito(self, circuito: list):
+        if circuito[0] != circuito[len(circuito)-1]:
+            return False
+        else:
+            return self.trilha(circuito)
+
+    def verifica(self, percurso):
+        verifica = []
+        verifica2 = []
+        for i in range(len(percurso) - 1):
+            if not self.adj_list[percurso[i]].__contains__(percurso[i + 1]):
+                return False
+            else:
+                if verifica.__contains__([percurso[i], percurso[i + 1]]) or verifica.__contains__(
+                        [percurso[i + 1], percurso[i]]) or verifica2.__contains__(percurso[i]):
+                    return False
+            verifica.append([percurso[i], percurso[i + 1]])
+            verifica2.append(percurso[i])
+        return True
+
+    def caminho(self, caminho: list):
+        if caminho[0] != caminho[len(caminho)-1]:
+            return self.verifica(caminho)
+        return False
+
+    def ciclo(self, ciclo: list):
+        if ciclo[0] == ciclo[len(ciclo) - 1]:
+            return self.verifica(ciclo)
+        else:
+            return False
